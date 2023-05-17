4d34f9e7428a20cba5386b0efa0f7ef289cc1ed2
ngenohkevin <ngenohkevin19@gmail.com>
2023-05-17 13:36:21 +0300

    getting started

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,13 +1,15 @@
-import React, {Component} from 'react';
+import styles from "../style.js";
 
-class GetStarted extends Component {
-    render() {
-        return (
-            <div>
-                GetStarted
+const GetStarted = () => (
+    <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
+        <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
+            <div className={`${styles.flexStart} flex-row `}>
+                <p>
+                    <span></span>
+                </p>
             </div>
-        );
-    }
-}
+        </div>
+    </div>
+);
 
 export default GetStarted;
