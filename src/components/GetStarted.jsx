864c6d86d0343b7801503631d05c6afebcb39979
ngenohkevin <ngenohkevin19@gmail.com>
2023-05-18 15:04:49 +0300

    free quote

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,13 +1,18 @@
 import styles from "../style.js";
+import {arrowUp} from "../assets/index.js";
 
 const GetStarted = () => (
     <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
         <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
             <div className={`${styles.flexStart} flex-row `}>
-                <p>
-                    <span></span>
+                <p className="font-poppins font-medium text-[18px] leading-[23px] mr-2">
+                    <span className="text-gradient">Get a</span>
                 </p>
+                <img src={arrowUp} alt="arrow" className="w-[23px] h-[23px] object-contain"/>
             </div>
+            <p className="font-poppins font-medium text-[18px] leading-[23px]">
+                <span className="text-gradient">Free Quote</span>
+            </p>
         </div>
     </div>
 );
