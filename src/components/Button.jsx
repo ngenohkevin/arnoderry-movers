e70dd224dd070890847a8c7037fdd4796332fd9d
ngenohkevin <ngenohkevin19@gmail.com>
2023-05-22 16:28:02 +0300

    new button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 
 // eslint-disable-next-line react/prop-types
-const Button = ({ styles }) => (
+const Button = ({ styles, name}) => (
     <button type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
-        Get Started
+        {name}
     </button>
 );
 
