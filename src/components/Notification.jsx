6fcc4acc004fb720d1ea9ff65307d6ed8666adad
ngenohkevin <ngenohkevin19@gmail.com>
2023-05-26 17:10:38 +0300

    notifications

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,13 +1,35 @@
-
+import { useState, useEffect } from 'react';
 
 // eslint-disable-next-line react/prop-types
-const Notification = ({ message, isError }) => {
-    const notificationClassName = isError ? 'bg-red-500' : 'bg-green-500';
-    const textColorClassName = isError ? 'text-white' : 'text-black';
+const Notification = ({ message, type, timeout }) => {
+    const [showNotification, setShowNotification] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowNotification(false);
+        }, timeout);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [timeout]);
+
+    if (!showNotification) {
+        return null;
+    }
+
+    let notificationClassName = 'fixed top-0 left-1/2 transform -translate-x-1/2 w-64 p-4 rounded-md';
+    if (type === 'success') {
+        notificationClassName += ' bg-green-500 text-white';
+    } else if (type === 'error') {
+        notificationClassName += ' bg-red-500 text-white';
+    } else {
+        notificationClassName += ' bg-gray-500 text-white';
+    }
 
     return (
-        <div className={`py-2 px-4 rounded-md ${notificationClassName} ${textColorClassName}`}>
-            {message}
+        <div className={notificationClassName}>
+            <p>{message}</p>
         </div>
     );
 };
