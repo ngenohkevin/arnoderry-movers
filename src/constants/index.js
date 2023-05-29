ab63962fce4e0d173a200c58cb924f4134589bdf
ngenohkevin <ngenohkevin19@gmail.com>
2023-05-29 21:23:28 +0300

    deleted

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,10 +6,7 @@ import {
     facebook,
     call,
     instagram,
-    airbnb,
     binance,
-    coinbase,
-    dropbox,
     shipping,
     cleaning, send, tiktok, youtube, whatsapp
 } from "../assets";
@@ -253,21 +250,21 @@ export const socialMedia = [
     }
 ];
 
-export const clients = [
-    {
-        id: "client-1",
-        logo: airbnb,
-    },
-    {
-        id: "client-2",
-        logo: binance,
-    },
-    {
-        id: "client-3",
-        logo: coinbase,
-    },
-    {
-        id: "client-4",
-        logo: dropbox,
-    },
-];
+// export const clients = [
+//     {
+//         id: "client-1",
+//         logo: airbnb,
+//     },
+//     {
+//         id: "client-2",
+//         logo: binance,
+//     },
+//     {
+//         id: "client-3",
+//         logo: coinbase,
+//     },
+//     {
+//         id: "client-4",
+//         logo: dropbox,
+//     },
+// ];
